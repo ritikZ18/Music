142c91a8cc31a6fd15968f00fad16933c3e522cc
ritikZ18 <116812243+ritikZ18@users.noreply.github.com>
2022-12-14 11:45:20 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,200 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Roobaro [Rang de Basanti]",
+      artist: "A R Rahman",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Kabhi Neem Neem [Yuva-2004]",
+      artist: "MadhuShree & A R Rahman",
+      img: "music-2",
+      src: "music-2"
+    },
+     {
+       name: "Genda Phool [Delhi-6]",
+       artist: "Sujata, Mahathi, Shraddha, Rekha",
+       img: "music-3",
+       src: "music-3"
+     },
+     {
+       name: "Barso Re",
+       artist: "Shreya Ghoshal & Uday Majumdar",
+       img: "music-4",
+       src: "music-4"
+     },
+     {
+       name: "Laapata [Ek Tha Tiger]",
+       artist: "KK",
+       img: "music-5",
+       src: "music-5"
+     },
+     {
+       name: "Rozana [Naam Shabana]",
+       artist: "Shreya Rochak",
+       img: "music-6",
+       src: "music-6"
+     },
+     {
+      name: "Roop Tera Mastana [Aradhana 1969]",
+      artist: "Kishore Kumar",
+      img: "music-7",
+      src: "music-7"
+    },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-8",
+    //   src: "music-8"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-9",
+    //   src: "music-9"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-10",
+    //   src: "music-10"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-11"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-12"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-13"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-14"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-15"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-16"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-17"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-18"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-19"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-20"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-21"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-22"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-23"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-24"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-25"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-26"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-27"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-28"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-29"
+    // },
+    // {
+    //   name: "Lost Sky - Vision NCS",
+    //   artist: "NCS Release",
+    //   img: "music-6",
+    //   src: "music-30"
+    // },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
